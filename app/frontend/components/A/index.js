b36f2a4e4186ca94a1f0fa4e6c43583a51257bf0
tinavanschelt <tinavanschelt@gmail.com>
2018-04-25 11:14:15 +0200

    revisit <A>

--- app/frontend/components/A/index.js
+++ app/frontend/components/A/index.js
@@ -5,10 +5,10 @@
 import styled from 'styled-components';
 
 const A = styled.a`
-  color: #41addd;
+  color: ${props => props.theme.colorGreen.light};
 
   &:hover {
-    color: #6cc0e5;
+    color: ${props => props.theme.colorGreen.tint};
   }
 `;
 
