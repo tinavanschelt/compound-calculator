2bd46bf83485bb597d51c03e484480b021c025ed
tinavanschelt <tinavanschelt@gmail.com>
2018-04-26 00:11:25 +0200

    finish off App and Calculator tests

--- app/frontend/containers/App/tests/index.test.js
+++ app/frontend/containers/App/tests/index.test.js
@@ -4,22 +4,32 @@
 
 import React from 'react';
 import { Provider } from 'react-redux';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../../theme';
 
 import App from '../index';
+import Calculator from '../../Calculator';
+import Graph from '../../Graph';
 
 import configureStore from '../../../store';
 
 describe('<App />', () => {
   const store = configureStore({});
 
-  const renderedComponent = shallow(
+  const renderedComponent = mount(
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   );
 
-  it('should render a div', () => {
-    expect(renderedComponent.find('div').length).toEqual(1);
+  it('should render the Calculator component', () => {
+    expect(renderedComponent.find(Calculator).length).toEqual(1);
+  });
+
+  it('should render the Graph component', () => {
+    expect(renderedComponent.find(Graph).length).toEqual(1);
   });
 });
--- app/frontend/containers/Calculator/tests/index.test.js
+++ app/frontend/containers/Calculator/tests/index.test.js
@@ -1,3 +1,76 @@
 /**
- * Tests for Calculator
+ * Tests for Calculator Container
  */
+
+import React from 'react';
+import { Provider } from 'react-redux';
+import { mount } from 'enzyme';
+import { ThemeProvider } from 'styled-components';
+import theme from '../../../theme';
+
+import Calculator, { mapDispatchToProps } from '../index';
+import { setCalculatedData, setConvertedData } from '../actions';
+
+import configureStore from '../../../store';
+
+const data = [
+  { month: 0, total: 100 },
+  { month: 1, total: 110 },
+  { month: 2, total: 120 }
+];
+
+describe('<Calculator />', () => {
+  const store = configureStore({});
+
+  const renderedComponent = mount(
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <Calculator />
+      </ThemeProvider>
+    </Provider>
+  );
+
+  it('should render a form tag', () => {
+    expect(renderedComponent.find('form').length).toEqual(1);
+  });
+
+  it('should render the input tags', () => {
+    expect(renderedComponent.find('input').length).toEqual(4);
+  });
+
+  it('should have a submit button', () => {
+    expect(renderedComponent.find('button').prop('type')).toBe('submit');
+  });
+});
+
+describe('mapDispatchToProps', () => {
+  describe('onSetCalculatedData', () => {
+    it('should be injected', () => {
+      const dispatch = jest.fn();
+      const result = mapDispatchToProps(dispatch);
+      expect(result.onSetCalculatedData).toBeDefined();
+    });
+
+    it('should dispatch setCalculatedData when called', () => {
+      const dispatch = jest.fn();
+      const result = mapDispatchToProps(dispatch);
+      result.onSetCalculatedData(data);
+      expect(dispatch).toHaveBeenCalledWith(setCalculatedData(data));
+    });
+  });
+
+  describe('onSetConvertedData', () => {
+    it('should be injected', () => {
+      const dispatch = jest.fn();
+      const result = mapDispatchToProps(dispatch);
+      expect(result.onSetConvertedData).toBeDefined();
+    });
+
+    it('should dispatch setConvertedData when called', () => {
+      const dispatch = jest.fn();
+      const result = mapDispatchToProps(dispatch);
+      result.onSetConvertedData(data);
+      expect(dispatch).toHaveBeenCalledWith(setConvertedData(data));
+    });
+  });
+});
